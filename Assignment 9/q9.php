c5a1105562ab732a08eab4e806b302cf6765a327
inba762 <inbaraj.s2023@vitstudent.ac.in>
2025-09-26 15:35:17 +0530

    Update q9.php

--- Assignment 9/q9.php
+++ Assignment 9/q9.php
@@ -4,7 +4,7 @@ if (!file_exists($originalFile)) {
     die("Error: $originalFile not found!");
 }
 $pathInfo = pathinfo($originalFile);
-$filename = $pathInfo['filename']; // "data"
+$filename = $pathInfo['filename']; 
 $extension = isset($pathInfo['extension']) ? "." . $pathInfo['extension'] : "";
 $timestamp = date("Y-m-d_H-i");
 $backupFile = $filename . "_" . $timestamp . $extension;
